07ca2a09fca0ec970389b8a72553e03b9b5ca2ad
nowex35 <a.hayate0726@gmail.com>
2025-09-20 18:58:25 +0900

    Diaryページの改善

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
+import remarkLinkCard from 'remark-link-card';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [
+      [remarkLinkCard, {
+        // カスタム設定
+        cache: true, // キャッシュを有効化
+        shortenUrl: true, // URLを短縮表示
+        target: '_blank', // 新しいタブで開く
+      }]
+    ]
+  },
   build: {
     assets: 'assets'
   },
